b005aa4fffb724c03cb9dcddd738aeac34bd3424
maryamjahanbakhshi <maryamjahanbakhshi008@gmail.com>
2025-12-13 23:20:26 +0330

    complect addbook

--- src/app/private/bookspage/bookspage.ts
+++ src/app/private/bookspage/bookspage.ts
@@ -1,25 +1,53 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Bookservice } from './bookservice';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bookspage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './bookspage.html',
   styleUrl: './bookspage.scss',
 })
 export class Bookspage implements OnInit {
+
+  Save() {
+    this.bookservice.Add(this.ItemB);
+    this.DataRefresh();
+    this.state='list';
+
+  }
+
   ngOnInit(): void {
     this.DataRefresh()
   }
+
+  state: string = 'list';
+
   data: BoolItem[] = [];
-  bookservice = inject(Bookservice)
+
+  ItemB: BoolItem = {
+    id: 0,
+    titel: '',
+    auther: '',
+    publisher: '',
+    price: 0
+  };
+
+  bookservice = inject(Bookservice);
+
   DataRefresh() {
     this.data = this.bookservice.list();
   }
+
   add() {
-    this.DataRefresh();
+    this.state = "Add";
+  }
+
+  cancel() {
+    this.state = 'list';
   }
 }
+
 export interface BoolItem {
   id: number;
   titel: string;
